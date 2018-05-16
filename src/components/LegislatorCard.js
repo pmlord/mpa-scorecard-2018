2e01c9648aff61722a289cec5d412a9ad762aa37
Peter Lord <pmlord@gmail.com>
2018-05-16 12:59:11 -0400

    legislator card badge

--- src/components/LegislatorCard.js
+++ src/components/LegislatorCard.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 import LegislatorInfo from './LegislatorInfo'
 
+const badgeTexts = [
+  { Senate: 'Senator', House: 'Representative' },
+  { Senate: 'Your Senator', House: 'Your Representative' },
+]
+
 export default function LegislatorCard(props) {
-  const { legislator } = props
+  const {
+    legislator,
+    your,
+  } = props
+  const { legislative_chamber } = legislator
+
+  const badgeText = badgeTexts[Number(your)][legislative_chamber]
 
   return (
     <div className="col-md-6 col-xs-12"><div className="box">
-      <div className="legislator-card card">
+      <div className={`legislator-card card ${your ? 'your' : ''}`}>
+        <div class="badge">{badgeText}</div>
         <LegislatorInfo legislator={legislator} compact />
       </div>
     </div></div>
--- src/components/Legislators.js
+++ src/components/Legislators.js
@@ -7,15 +7,23 @@ import LegislatorCard from './LegislatorCard'
 
 export default function(props) {
 
-  const visibleLegislators = sample(legislators, 5)
+  const yourLigislators = sample(legislators, 2)
+  const visibleLegislators = sample(legislators, 10)
 
   // Array of legislator cards
+  const yourLegislatorCards = yourLigislators.map((legislator) => {
+    return <LegislatorCard legislator={legislator} your={true} key={legislator.ocdId} />
+  })
   const legislatorCards = visibleLegislators.map((legislator) => {
-    return <LegislatorCard legislator={legislator} key={legislator.ocdId} />
+    return <LegislatorCard legislator={legislator} your={false} key={legislator.ocdId} />
   })
 
   return (
     <div className="legislator-cards">
+      <div className="row">
+        {yourLegislatorCards}
+      </div>
+      <br /><br />
       <div className="row">
         {legislatorCards}
       </div>
