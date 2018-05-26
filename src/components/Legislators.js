56fe79686da3c2c1953197b80efe5bd6e41da4a6
Peter Lord <pmlord@gmail.com>
2018-05-26 15:04:25 -0400

    add a "show others" button. Loads too many items and takes too long on click hanlder

--- src/components/Legislators.js
+++ src/components/Legislators.js
@@ -4,33 +4,70 @@ import LegislatorCard from './LegislatorCard'
 import withStore from '../services/legislators-store'
 
 
-export default withStore(function Legislators(props) {
-  const { store } = props
+class Legislators extends React.Component {
+  constructor(props) {
+    super()
 
-  const yourLegislators = store.get('yourLegislators')
-  // const otherLegislators = store.get('otherLegislators')
+    this.state = {
+      showOthers: false,
+    }
+  }
 
-  // Array of legislator cards
-  const yourLegislatorCards = yourLegislators.map((legislator) => {
-    return <LegislatorCard legislator={legislator} your={true} key={legislator.ocdId} />
-  })
-  // const otherLegislatorCards = otherLegislators.map((legislator) => {
-  //   return <LegislatorCard legislator={legislator} your={false} key={legislator.ocdId} />
-  // })
+  toggleShowOthers = () => {
+    const prevState = this.state.showOthers
 
-  return (
-    <div className="legislators">
-      <FindMyLegislators />
-      { yourLegislators.length === 0 &&
-        <h1 className="zero-state-message">
-          Enter your address to find your representatives.
-        </h1>
-      }
-      <div className="legislator-cards">
-        <div className="row">
-          {yourLegislatorCards}
+    this.setState({
+      showOthers: !prevState
+    })
+  }
+
+  render() {
+    const { store } = this.props
+    const { showOthers } = this.state
+
+    const yourLegislators = store.get('yourLegislators')
+    const otherLegislators = store.get('otherLegislators')
+
+    // Render legislator cards
+    const yourLegislatorCards = yourLegislators.map((legislator) => {
+      return <LegislatorCard legislator={legislator} your={true} key={legislator.ocdId} />
+    })
+    const otherLegislatorCards = showOthers
+      ? (
+        otherLegislators.map((legislator) => {
+          return <LegislatorCard legislator={legislator} your={false} key={legislator.ocdId} />
+        })
+      )
+      : []
+
+    return (
+      <div className="legislators">
+        <FindMyLegislators />
+        { yourLegislators.length === 0 &&
+          <h1 className="zero-state-message">
+            Enter your address to find your representatives.
+          </h1>
+        }
+        <div className="legislator-cards">
+          <div className="row">
+            {yourLegislatorCards}
+          </div>
         </div>
+        <div className="showOthers">
+          <button onClick={this.toggleShowOthers} >
+            {showOthers ? 'Hide' : 'Show'} Others
+          </button>
+        </div>
+        { showOthers &&
+          <div className="legislator-cards">
+            <div className="row">
+              {otherLegislatorCards}
+            </div>
+          </div>
+        }
       </div>
-    </div>
-  )
-});
+    )
+  }
+}
+
+export default withStore(Legislators);
