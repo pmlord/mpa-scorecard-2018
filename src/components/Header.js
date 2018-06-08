c101e06152cd25f7263e89afb8c8e4d75dc9d9fa
Peter Lord <pmlord@gmail.com>
2018-06-08 10:23:15 -0400

    logo image -> text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/images/mpa-logo.png'
 
 export default function Header(props) {
   return (
     <div className="header">
       <div className="banner container">
         <Link to="/" className="brand">
-          <img src={logo} alt="" />
+          Maine People's Alliance
         </Link>
-        &nbsp;
-        <span className="scorecard-title"> 2018&nbsp;Legislative&nbsp;Scorecard</span>
+        <span className="scorecard-title">2018 Legislative Scorecard</span>
       </div>
       <nav className="container">
         <NavLink to="/legislators" activeClassName="active">Legislators</NavLink>
